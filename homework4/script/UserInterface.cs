02d30d8a647ce9ba73add4a82e712711594cf54a
XuYanQing <37723745+XuYanQing@users.noreply.github.com>
2018-04-16 23:43:58 +0800

    Add files via upload

---
+++ homework4/script/UserInterface.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using PlayDarts.Com;  
+
+public class UserInterface : MonoBehaviour {  
+	private IUserAction action;  
+
+	void Start () {  
+		action = MainSceneController.getInstance() as IUserAction;  
+
+	}  
+
+	void Update () {  
+		detectSpaceKeyAndLaunchDarts();  
+		detectMouseDownAndStrikeTheDart();  
+	}  
+
+	void detectSpaceKeyAndLaunchDarts() {  
+		if (Input.GetKeyDown(KeyCode.Space)) {  
+			action.launchDarts();  
+		}  
+	}  
+
+	void detectMouseDownAndStrikeTheDart() {  
+		if (Input.GetMouseButtonDown(0)) {  
+			Vector3 mouseWorldPosition = Input.mousePosition;  
+			action.strikeTheDart(mouseWorldPosition);  
+		}  
+	}  
+}  
