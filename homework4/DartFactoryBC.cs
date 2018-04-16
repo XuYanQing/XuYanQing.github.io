65ce04c29f67319f18740a3802141f83b6d2741d
XuYanQing <37723745+XuYanQing@users.noreply.github.com>
2018-04-16 21:59:39 +0800

    Add files via upload

---
+++ homework4/DartFactoryBC.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using PlayDarts.Com;  
+
+namespace PlayDarts.Com {  
+	public class DartFactory : System.Object {  
+		private static DartFactory instance;  
+		private List<GameObject> usingDartList = new List<GameObject>();   //正在使用的飞镖list  
+		private List<GameObject> unusedDartList = new List<GameObject>();  //没有使用的飞镖list  
+
+		private GameObject dartItem;  
+
+		public static DartFactory getInstance() {  
+			if (instance == null)  
+				instance = new DartFactory();  
+			return instance;  
+		}  
+
+		//提供飞镖  
+		public GameObject getDart() {  
+			if (unusedDartList.Count == 0) {    //没有存储飞镖  
+				GameObject newDart = Camera.Instantiate(dartItem);  
+				usingDartList.Add(newDart);  
+				return newDart;  
+			}  
+			else {                      //有存储飞镖  
+				GameObject oldDart = unusedDartList[0];  
+				unusedDartList.RemoveAt(0);  
+				oldDart.SetActive(true);  
+				usingDartList.Add(oldDart);  
+				return oldDart;  
+			}  
+		}  
+
+		//update()检测飞镖落地，回收。此方法由GameModel的update()方法触发  
+		public void detectReuseDarts() {  
+			for (int i = 0; i < usingDartList.Count; i++) {  
+				if (usingDartList[i].transform.position.y <= -8) {  
+					usingDartList[i].GetComponent<Rigidbody>().velocity = Vector3.zero;  //很重要  
+					usingDartList[i].SetActive(false);  
+					unusedDartList.Add(usingDartList[i]);  
+					usingDartList.Remove(usingDartList[i]);  
+					i--;  
+
+					MainSceneController.getInstance().subScore();  //打不中，扣分  
+				}  
+			}  
+		}  
+
+		//飞镖被击中，回收  
+		public void ReuseWhenDartBeingStruck(GameObject StruckDart) {  
+			StruckDart.GetComponent<Rigidbody>().velocity = Vector3.zero;  //很重要  
+			StruckDart.SetActive(false);  
+			unusedDartList.Add(StruckDart);  
+			usingDartList.Remove(StruckDart);  
+		}  
+
+		//告知是否正在发射飞镖：若是则不能重复发射  
+		public bool isLaunching() {  
+			return (usingDartList.Count > 0);  
+		}  
+
+		//初始化dartItem  
+		public void initItems(GameObject _dartItem) {  
+			dartItem = _dartItem;  
+		}  
+	}  
+}  
+
+public class DartFactoryBC : MonoBehaviour {  
+	public GameObject dartItem;  
+
+	void Awake() {  
+		DartFactory.getInstance().initItems(dartItem);  
+	}  
+}  
---
+++ homework4/GameStatus.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using PlayDarts.Com;  
+using UnityEngine.UI;  
+
+public class GameStatus : MonoBehaviour {  
+	public GameObject canvasItem, roundTextItem, scoreTextItem, TipsTextItem;  
+
+	private int roundNum = 1;  
+	private int score = 0;  
+
+	private const float TIPS_TEXT_SHOW_TIME = 0.8f;  
+
+	private GameObject canvas, roundText, scoreText, TipsText;  
+	private MainSceneController scene;  
+
+	void Start () {  
+		scene = MainSceneController.getInstance();  
+		scene.setGameStatus(this);  
+
+		canvas = Instantiate(canvasItem);  
+		roundText = Instantiate(roundTextItem, canvas.transform);  
+		roundText.transform.Translate(canvas.transform.position);  
+		roundText.GetComponent<Text>().text = "Round: " + roundNum;  
+
+		scoreText = Instantiate(scoreTextItem, canvas.transform);  
+		scoreText.transform.Translate(canvas.transform.position);  
+		scoreText.GetComponent<Text>().text = "Score:  " + score + " / " + (roundNum * 100);  
+
+		TipsText = Instantiate(TipsTextItem, canvas.transform);  
+		TipsText.transform.Translate(canvas.transform.position);  
+		showTipsText();  
+	}  
+
+	void Update () {  
+
+	}  
+
+	public int getRoundNum() {  
+		return roundNum;  
+	}  
+
+	void addRoundNum() {  
+		roundNum++;  
+		roundText.GetComponent<Text>().text = "Round: " + roundNum;  
+	}  
+
+	public int getScore() {  
+		return score;  
+	}  
+
+	//得分，+100  
+	public void addScore() {  
+		score += 100;  
+		scoreText.GetComponent<Text>().text = "Score:  " + score + " / " + (roundNum * 100);  
+		checkScore();  
+	}  
+
+	//扣分，-100  
+	public void subScore() {  
+		score = score >= 100 ? score - 100 : 0;  
+		scoreText.GetComponent<Text>().text = "Score:  " + score + " / " + (roundNum * 100);  
+	}  
+
+	//检测分数是否已满  
+	void checkScore() {  
+		if (score >= roundNum * 100) {  //可以下一关  
+			addRoundNum();  
+			resetScore();  
+
+			showTipsText();  
+		}  
+	}  
+	void resetScore() {  
+		score = 0;  
+		scoreText.GetComponent<Text>().text = "Score:  " + score + " / " + (roundNum * 100);  
+	}  
+	void showTipsText() {  
+		TipsText.GetComponent<Text>().text = "Round " + roundNum + " !";  
+		TipsText.SetActive(true);  
+		StartCoroutine(waitForSomeAndDisappearTipsText());  
+	}  
+	IEnumerator waitForSomeAndDisappearTipsText() {  
+		yield return new WaitForSeconds(TIPS_TEXT_SHOW_TIME);  
+
+		TipsText.SetActive(false);  
+	}  
+}  
+
